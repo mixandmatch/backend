4af9a554f00e53d1c5cae3cba8622165ea1d7c8d
metaphil <philipp.bichlmaier@metafinanz.de>
2013-12-06 10:05:58 +0100

    Delete createTables.sql

--- doc/createTables.sql
+++
@@ -1,9 +0,0 @@
-Drop table if exists users;
-Drop table if exists locations;
-Drop table if exists appointments;
-Drop table if exists matches;
-
-create table users(id int primary key, name varchar(30));
-create table locations(id int primary key, name varchar(30));
-create table appointments(id int primary key, appointmentDate datetime, locationID int, userID int);
-create table matches(id int primary key, userID int, appointmentID int);
