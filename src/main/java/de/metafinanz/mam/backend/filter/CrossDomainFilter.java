89a1e21fd62b133d9ab477c10cc68ba1c041a389
metaphil <philipp.bichlmaier@metafinanz.de>
2014-01-28 20:38:24 +0100

    CrossDomainFilter

---
+++ src/main/java/de/metafinanz/mam/backend/filter/CrossDomainFilter.java
@@ -0,0 +1,17 @@
+package de.metafinanz.mam.backend.filter;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+public class CrossDomainFilter implements ContainerResponseFilter {
+   
+    @Override
+    public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
+    	response.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+    	response.getHttpHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+    	response.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
+    	response.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        return response;
+    }
+}
